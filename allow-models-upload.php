6f59e1339a12d65cbe475f7ca5b14b7bf7038b55
JimJ92120 <57893611+JimJ92120@users.noreply.github.com>
2023-01-16 13:08:23 +0700

    chore(init): add plugin entrypoint with headers

---
+++ allow-models-upload.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name:         Allow Models Upload
+ * 
+ * Description:         Allow <em>.gltf</em> and <em>.glb</em> files to be uploaded into WordPress Media Library.
+ * Author:              JimJ92120
+ * Author URI:          https://github.com/JimJ92120
+ * 
+ * Version:             0.1.0
+ * Requires at least:   5.9
+ * Requires PHP:        7.4
+ */
